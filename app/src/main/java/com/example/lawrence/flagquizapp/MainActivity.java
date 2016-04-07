1cd814dd3144e87b4ab184a5923d63798c9af901
Lawrence L <Lawrence.Lee@alumni.ucsd.edu>
2016-04-07 15:27:50 -0700

    Completed implementing layout files.

--- app/src/main/java/com/example/lawrence/flagquizapp/MainActivity.java
+++ app/src/main/java/com/example/lawrence/flagquizapp/MainActivity.java
@@ -1,7 +1,21 @@
 package com.example.lawrence.flagquizapp;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.preference.PreferenceManager;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
