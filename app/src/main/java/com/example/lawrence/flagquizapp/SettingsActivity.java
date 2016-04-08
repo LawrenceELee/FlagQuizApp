a87033bed2f9a4620d402d84ecac0d4e5db65ec4
Lawrence L <Lawrence.Lee@alumni.ucsd.edu>
2016-04-08 16:46:34 -0700

    Completed settings related code (layout, logic, etc).

--- app/src/main/java/com/example/lawrence/flagquizapp/SettingsActivity.java
+++ app/src/main/java/com/example/lawrence/flagquizapp/SettingsActivity.java
@@ -9,21 +9,13 @@ import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    // inflates GUI, displays Toolbar and "up" button to go to previous activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
